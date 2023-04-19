b09f19e2fe01ecc1a21fcbb869ea81e971137848
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2023-04-19 11:27:45 +0530

    Update userbot.py

--- AnonX/core/userbot.py
+++ AnonX/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("https://t.me/TKS_CHAT_OFFICIAl")
-                await self.one.join_chat("https://t.me/TKS_COME")
+                await self.one.join_chat("TKS_CHAT_OFFICIAl")
+                await self.one.join_chat("TKS_COME")
             except:
                 pass
             assistants.append(1)
@@ -79,8 +79,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("DevilsHeavenMF")
-                await self.two.join_chat("FallenAssociation")
+                await self.two.join_chat("TKS_CHAT_OFFICIAL")
+                await self.two.join_chat("TKS_COME")
             except:
                 pass
             assistants.append(2)
@@ -110,8 +110,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("https://t.me/TKS_CHAT_OFFICIAL")
-                await self.three.join_chat("https://t.me/TKS_COME")
+                await self.three.join_chat("TKS_CHAT_OFFICIAL")
+                await self.three.join_chat("TKS_COME")
             except:
                 pass
             assistants.append(3)
@@ -141,8 +141,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("https://t.me/TKS_CHAT_OFFICIAL")
-                await self.four.join_chat("https://t.me/TKS_COME")
+                await self.four.join_chat("TKS_CHAT_OFFICIAL")
+                await self.four.join_chat("TKS_COME")
             except:
                 pass
             assistants.append(4)
@@ -172,8 +172,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("https://t.me/TKS_CHAT_OFFICIAL")
-                await self.five.join_chat("https://t.me/TKS_COME")
+                await self.five.join_chat("TKS_CHAT_OFFICIAL")
+                await self.five.join_chat("TKS_COME")
             except:
                 pass
             assistants.append(5)
